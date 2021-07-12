f8b0f01bcf24da8e72efa93e58b201d4243f60c3
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-12 18:21:19 +0300

    Add the store modal

---
+++ models/Store.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const storeSchema = new Schema({
+    storeId: {
+        type: String,
+        required: [true, "Please add a store ID"],
+        unique: true,
+        trim: true,
+        maxLength: [10, "Store ID must less or equal to 10 chars"]
+    },
+    location: {
+        type: {
+          type: String,
+          enum: ['Point']
+        },
+        coordinates: {
+          type: [Number],
+          index: "2dsphere"
+        },
+        formattedAddress: String
+      },
+    //   createdAt: {
+    //       type: Date,
+    //       default: Date.now
+    //   }
+}, {timestamps: true})
+
+module.exports = mongoose.model("Store", storeSchema)
