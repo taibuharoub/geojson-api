d6d6fa5aa96dcba0fd967873a19abc4805ba9a21
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-12 20:29:51 +0300

    Add monoogse middleware to geocode address

--- models/Store.js
+++ models/Store.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
+const geocoder = require("../utils/geocoder");
+
 const storeSchema = new Schema({
     storeId: {
         type: String,
@@ -29,5 +31,22 @@ const storeSchema = new Schema({
     //       default: Date.now
     //   }
 }, {timestamps: true})
+
+//Add Mongoose middleware to handle geocoding
+//Geocode & create location
+//Normal normal function not an arrow function
+storeSchema.pre("save", async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  // console.log(loc);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress
+  }
+
+  //Do not save address (entered user address)
+  this.address = undefined;
+  next();
+})
 
 module.exports = mongoose.model("Store", storeSchema)
