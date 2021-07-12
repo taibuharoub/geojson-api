7c20ea5cfdf1140d10d18541fff3504170df034b
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-12 18:35:28 +0300

    Add controller to get all stores

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,3 +1,15 @@
-exports.getStores = (req, res, next) => {
-  res.status(200).json({ stores: "Avaiable stores" });
+const Store = require("../models/Store");
+
+exports.getStores = async (req, res, next) => {
+  try {
+    const stores = await Store.find();
+    return res.status(200).json({
+      success: true,
+      count: stores.length,
+      data: stores,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
 };
